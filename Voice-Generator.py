b519096d963a78aaff2d15c832ae0803d43313ca
SamuelLindao <lacerdasamuel26@gmail.com>
2026-04-22 15:17:00 -0300

    files

--- Voice-Generator.py
+++ Voice-Generator.py
@@ -1,12 +1,14 @@
-import os
 import subprocess
 import uuid
+import os
 
-PIPER = r"C:\piper\piper.exe"
-MODEL = r"voice-model\pt_BR-faber-medium.onnx"
+PIPER = "piper"
+MODEL = "./voice-model/pt_BR-faber-medium.onnx"
 
 def gerar_e_tocar(texto):
-    out_file = f"aud\out_{uuid.uuid4().hex}.wav"
+    os.makedirs("aud", exist_ok=True)
+
+    out_file = f"aud/out_{uuid.uuid4().hex}.wav"
 
     subprocess.run([
         PIPER,
@@ -14,9 +16,9 @@ def gerar_e_tocar(texto):
         "--output_file", out_file
     ], input=texto, text=True)
 
-    # toca o áudio
-    subprocess.Popen(["start", out_file], shell=True)
+    subprocess.Popen(["aplay", out_file])
 
     return out_file
 
-gerar_e_tocar("Oi Samuel ama Raquel muito")
+
+gerar_e_tocar("Oi, rodando no servidor")
